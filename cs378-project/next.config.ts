55da8f3209b9ce40afc72eab2260b51fb701c0a2
Daniyal <31736868+daniyal-d@users.noreply.github.com>
2025-03-31 22:26:26 -0500

    pls

--- cs378-project/next.config.ts
+++ cs378-project/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+// const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export'
+  output: 'export',
+  basePath: "/cs378-project",
+  assetPrefix: "/cs378-project/",
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
